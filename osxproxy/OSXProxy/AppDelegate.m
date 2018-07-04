5ae590b322a3fdeaa6d42cdf7cb0845233cd4aa6
Don Porter <porter@cs.unc.edu>
2018-07-04 15:24:38 -0400

    Add standard license/copyright header to source files.

--- osxproxy/OSXProxy/AppDelegate.m
+++ osxproxy/OSXProxy/AppDelegate.m
@@ -1,10 +1,28 @@
-//
-//  AppDelegate.m
-//  NVRDP
-//
-//  Created by Syed Masum Billah on 7/31/14.
-//  Copyright (c) 2014 Stony Brook University. All rights reserved.
-//
+/* Copyright (C) 2014--2018 Stony Brook University
+   Copyright (C) 2016--2018 The University of North Carolina at Chapel Hill
+
+   This file is part of the Sinter Remote Desktop System.
+
+   Sinter is dual-licensed, available under a commercial license or
+   for free subject to the LGPL.  
+
+   Sinter is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.  Sinter is distributed in the
+   hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the GNU Lesser General Public License for more details.  You
+   should have received a copy of the GNU Lesser General Public License along
+   with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+    AppDelegate.m
+    NVRDP
+
+    Created by Syed Masum Billah on 7/31/14.
+*/
 
 #import "AppDelegate.h"
 #import "CustomWindowController.h"
--- osxscraper/OSXScraper/Scraper.h
+++ osxscraper/OSXScraper/Scraper.h
@@ -1,10 +1,28 @@
-//
-//  Scraper.h
-//  OSXScrapper
-//
-//  Created by Syed Masum Billah on 10/19/16.
-//  Copyright © 2016 Stony Brook University. All rights reserved.
-//
+/* Copyright (C) 2014--2018 Stony Brook University
+   Copyright (C) 2016--2018 The University of North Carolina at Chapel Hill
+
+   This file is part of the Sinter Remote Desktop System.
+
+   Sinter is dual-licensed, available under a commercial license or
+   for free subject to the LGPL.  
+
+   Sinter is free software: you can redistribute it and/or modify it
+   under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.  Sinter is distributed in the
+   hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+   the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the GNU Lesser General Public License for more details.  You
+   should have received a copy of the GNU Lesser General Public License along
+   with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+    Scraper.h
+    OSXScrapper
+  
+    Created by Syed Masum Billah on 10/19/16.
+*/
 
 #import <Foundation/Foundation.h>
 #include "ClientHandler.h"
