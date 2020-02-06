ca987c6f8d6fafd9b0d56c58d70321366aa41728
Erica Fu <ericafu@renci.org>
2020-02-06 11:20:45 -0500

    add notepad, wordpad as supported programs in unittest enviroment

--- windowscraper/ScraperTests/ScraperTest.cs
+++ windowscraper/ScraperTests/ScraperTest.cs
@@ -24,6 +24,8 @@ namespace WindowsScraper.Tests
         WindowsScraper scraper;
         CommandHandler cmdhlr;
         DummyConnection conn;
+        const string path_win7_calc = @"C:\Windows\System32\calc1.exe";
+        const string path_wordpad = @"C:\Program Files\windows nt\accessories\wordpad.exe";
 
         [TestInitialize]
         public void Initialize()
@@ -39,12 +41,14 @@ namespace WindowsScraper.Tests
             conn = new DummyConnection();
             conn.filepath = string.Format(@"{0}\{1}.xml", resultDir, TestContext.TestName); /* set connection filepath to desired output file */
             scraper.connection = conn;
+            scraper.supportedProcesses = new string[] { "calc1", "notepad", "wordpad" };
         }
 
         public Process bringupApp(string path)
         {
+            Thread.Sleep(3000);
             Process newprocess = Process.Start(path);
-            Thread.Sleep(2000);
+            Thread.Sleep(3000);
             return newprocess;
         }
 
@@ -52,14 +56,14 @@ namespace WindowsScraper.Tests
         {
             appProcess.CloseMainWindow();
             appProcess.Close();
+            Thread.Sleep(3000);
         }
 
 
         [TestMethod]
         public void Test001_ls_Win7_Calc()
         {
-            Process calc = bringupApp(@"C:\Windows\System32\calc1.exe");
-            Thread.Sleep(2000);
+            Process calc = bringupApp(path_win7_calc);
 
             /* create an input "ls_l" and execute it */
             Sinter sinter_ls_req = new Sinter
@@ -91,7 +95,7 @@ namespace WindowsScraper.Tests
         [TestMethod]
         public void Test002_ls_l_Win7_Calc_Standard()
         {
-            Process calc = bringupApp(@"C:\Windows\System32\calc1.exe");
+            Process calc = bringupApp(path_win7_calc);
             SendKeys.SendWait("%1"); // Key ALT+1 switch to Standard mode
             Thread.Sleep(2000);
 
@@ -110,7 +114,7 @@ namespace WindowsScraper.Tests
         [TestMethod]
         public void Test003_ls_l_Win7_Calc_Scientific()
         {
-            Process calc = bringupApp(@"C:\Windows\System32\calc1.exe");
+            Process calc = bringupApp(path_win7_calc);
             SendKeys.SendWait("%2"); // Key ALT+2 switch to Scientific mode
             Thread.Sleep(2000);
 
@@ -129,7 +133,7 @@ namespace WindowsScraper.Tests
         [TestMethod]
         public void Test004_ls_l_Win7_Calc_Programmer()
         {
-            Process calc = bringupApp(@"C:\Windows\System32\calc1.exe");
+            Process calc = bringupApp(path_win7_calc);
             SendKeys.SendWait("%3"); // Key ALT+3 switch to Programmer mode
             Thread.Sleep(2000);
 
@@ -148,9 +152,58 @@ namespace WindowsScraper.Tests
         [TestMethod]
         public void Test005_ls_l_Win7_Calc_Statistics()
         {
-            Process calc = bringupApp(@"C:\Windows\System32\calc1.exe");
+            Process calc = bringupApp(path_win7_calc);
             SendKeys.SendWait("%4"); // Key ALT+4 switch to Statistics mode
             Thread.Sleep(2000);
+
+            /* create an input "ls_l_req" and execute it */
+            Sinter sinter_ls_l_req = new Sinter
+            {
+                HeaderNode = MsgUtil.BuildHeader(scraper.serviceCodes["ls_l_req"]),
+                EntityNodes = null,
+            };
+            sinter_ls_l_req.HeaderNode.Process = calc.Id.ToString();
+
+            cmdhlr.CommandExecSinter(sinter_ls_l_req);
+            killProcess(calc);
+        }
+
+
+        [TestMethod]
+        public void Test101_ls_WordPad()
+        {
+            Process calc = bringupApp(path_wordpad);
+
+            /* create an input "ls_l" and execute it */
+            Sinter sinter_ls_req = new Sinter
+            {
+                HeaderNode = MsgUtil.BuildHeader(scraper.serviceCodes["ls_req"]),
+                EntityNodes = null,
+            };
+            cmdhlr.CommandExecSinter(sinter_ls_req);
+
+            /* get ls_res */
+            Sinter sinter_ls_res = conn.GetSinterFromFile(conn.filepath);
+
+            /* parse ls_res and find wordpad */
+            string calcProcess = null;
+            foreach (Entity proc in sinter_ls_res.EntityNodes)
+            {
+                Console.WriteLine(proc.Name + " " + proc.Process);
+                if (proc.Name.IndexOf("wordpad", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    calcProcess = proc.Process;
+                    break;
+                }
+            }
+            Assert.IsTrue(calcProcess != null); //to check if wordpad is in the "ls_res" result.
+            killProcess(calc);
+        }
+
+        [TestMethod]
+        public void Test102_ls_l_WordPad()
+        {
+            Process calc = bringupApp(path_wordpad);
 
             /* create an input "ls_l_req" and execute it */
             Sinter sinter_ls_l_req = new Sinter
